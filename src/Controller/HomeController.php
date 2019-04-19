6ce675d4187cd42c05570a7415a24d3d6ede4617
Mohamed El Moktar <md.mbarek89@gmail.com>
2019-04-19 20:18:35 +0200

    mes premières pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,15 @@
 // src/Controller/HomeController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-class HomeController
+class HomeController extends AbstractController
 {
+
     public function index()
     {
-        $number = random_int(0, 100);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('pages/home.html.twig');
     }
 }
